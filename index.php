d01b99d2169fd65c3d0000eff99bb6b4e47b7a90
renld22 <renaldimfauzi3@gmail.com>
2023-11-07 21:40:25 +0700

    tugas

--- index.php
+++ index.php
@@ -10,40 +10,33 @@ if(!isset($_SESSION['session_username'])){
 
 
 <!DOCTYPE html>
-
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pemograman3.com</title>
+
+    <!-- Add Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha2/dist/css/bootstrap.min.css">
+
     <style>
+        /* Your custom styles can go here if needed */
         body {
-            font-family: Arial, sans-serif;
+            font-Comic Sans MS: Arial, sans-serif;
             background-color: #f5f5f5;
-            margin: 0;
-            padding: 0;
-            
         }
 
-        
-             
-        
+        header {
+            background-color: #3498db;
+            color: white;
+            padding: 20px;
+            margin:20px;
+            text-align: center;
+        }
 
         .container {
             text-align: center;
             padding: 30px;
-        }
-
-        header {
-            background-color: #94bbe9;
-            color: white;
-            padding: 20px;
-        }
-
-        header h1 {
-            margin: 0;
         }
 
         .contain {
@@ -58,63 +51,88 @@ if(!isset($_SESSION['session_username'])){
             text-decoration: none;
         }
 
-        .btn {
-            background-color: #3498db;
-            color: white;
-            padding: 10px 20px;
-            border: none;
+        .card {
+            background-color: #fff;
+            border: 1px solid #ccc;
             border-radius: 5px;
+            padding: 20px;
             text-align: center;
-            transition: background-color 0.3s;
+            transition: background-color 0.3s, transform 0.2s;
         }
 
-        .btn:hover {
-            background-color: #e74c3c;
+        .card:hover {
+            background-color: #ADD8E6	;
+            color: #ADFF2F;
+            transform: scale(1.05);
+        }
+
+        h2 {
+            margin: 0;
+            color: #3498db;
+        }
+
+        footer {
+            background-color: #ffff;
+            color: black;
+            padding: 10px;
+            text-align: center;
         }
     </style>
 </head>
-
 <body>
+    <header>
+        <h1>MENU PEMOGRAMAN 3</h1>
+    </header>
     <div class="container">
-        <header>
-            <h1>MENU PEMOGRAMAN 3</h1>
-        </header>
         <div class="contain">
             <a href="tampil_barang.php">
-                <button class="btn">BARANG</button>
+                <div class="card">
+                    <h2>BARANG</h2>
+                </div>
             </a>
-
             <a href="tampil_kategori.php">
-                <button class="btn">KATEGORI</button>
+                <div class="card">
+                    <h2>KATEGORI</h2>
+                </div>
             </a>
-
             <a href="tampil_member.php">
-                <button class="btn">MEMBER</button>
+                <div class="card">
+                    <h2>MEMBER</h2>
+                </div>
             </a>
-
             <a href="tampil_penjualan.php">
-                <button class="btn">PENJUALAN</button>
+                <div class="card">
+                    <h2>PENJUALAN</h2>
+                </div>
             </a>
-
             <a href="tampil_transaksi.php">
-                <button class="btn">TRANSAKSI</button>
+                <div class="card">
+                    <h2>TRANSAKSI</h2>
+                </div>
             </a>
-
             <a href="tampil_user.php">
-                <button class="btn">USER</button>
+                <div class="card">
+                    <h2>USER</h2>
+                </div>
             </a>
-
             <a href="view_report.php">
-                <button class="btn">VIEW REPORT</button>
+                <div class="card">
+                    <h2>VIEW REPORT</h2>
+                </div>
             </a>
             <a href="logout.php">
-                <button class="btn">LOGOUT</button>
+                <div class="card">
+                    <h2>LOGOUT</h2>
+                </div>
             </a>
         </div>
-        <footer>
-            Created by Renaldi Muhammad Fauzi
-        </footer>
     </div>
-</body>
+    <footer>
+        Created by Renaldi Muhammad Fauzi
+    </footer>
 
+    <!-- Add Bootstrap JS and Popper.js for Bootstrap components that require it -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+</body>
 </html>
