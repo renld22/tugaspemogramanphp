d85565c3c1d52419dec8d4d6de4e06db9ed4a610
renld22 <renaldimfauzi3@gmail.com>
2023-11-21 05:58:19 +0700

    tugass

--- index.php
+++ index.php
@@ -13,14 +13,12 @@ if ($_SESSION['level'] == 0) {
     $welcomeMessage = 'Selamat datang, Admin!';
 }$welcomeMessage = '';
 if ($_SESSION['level'] == 2) {
-    $welcomeMessage = 'Selamat datang, Staff!';
+    $welcomeMessage = 'Selamat datang, staff!';
 }$welcomeMessage = '';
 if ($_SESSION['level'] == 3) {
     $welcomeMessage = 'Selamat datang, supervisor!';
-}$welcomeMessage = '';
-if ($_SESSION['level'] == 4) {
-    $welcomeMessage = 'Selamat datang, manager!';
 }
+
 
 ?>
 
