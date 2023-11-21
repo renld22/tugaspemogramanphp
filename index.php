22fdfa03b85ad3ff771a3878eddd2b443176eec6
renld22 <renaldimfauzi3@gmail.com>
2023-11-21 10:06:54 +0700

    tugas

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ if(!isset($_SESSION['session_username'])){
     header("location:login.php");
     exit();
 }
+$loggedInUserLevel = isset($_SESSION['level']) ? $_SESSION['level'] : null; // Ambil level user yang sedang login
 
 
 // Tambahkan pesan selamat datang untuk level 0
@@ -112,6 +113,11 @@ if ($_SESSION['level'] == 0) {
     </header>
     <div class="container">
         <div class="contain">
+            <?php
+            if ($loggedInUserLevel != 2) {
+    // Level 0 dapat mengedit dan menghapus semua level
+    
+            ?>
             <a href="tampil_barang.php">
                 <div class="card">
                     <h2>BARANG</h2>
@@ -133,12 +139,7 @@ if ($_SESSION['level'] == 0) {
                     <h2>PENJUALAN</h2>
                 </div>
             </a>
-            <a href="tampil_transaksi.php">
-                <div class="card">
-                    <h2>TRANSAKSI</h2>
-                </div>
-            </a>
-            <a href="tampil_user.php">
+                        <a href="tampil_user.php">
                 <div class="card">
                     <h2>USER</h2>
                 </div>
@@ -146,6 +147,14 @@ if ($_SESSION['level'] == 0) {
             <a href="view_report.php">
                 <div class="card">
                     <h2>VIEW REPORT</h2>
+                </div>
+            </a>
+            <?php
+            }
+            ?>
+            <a href="tampil_transaksi.php">
+                <div class="card">
+                    <h2>TRANSAKSI</h2>
                 </div>
             </a>
             <a href="logout.php">
